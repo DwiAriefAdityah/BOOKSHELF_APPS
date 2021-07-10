26bf0b609ca06844050e481fb418d2041f1b3e3c
Dwi <dwi.aditah007@gmail.com>
2021-07-10 22:41:50 +0700

    Fix Design Web

--- js/dom.js
+++ js/dom.js
@@ -26,15 +26,21 @@ function tambahData() {
 function buatData(title, pengarang, date, isCompleted) {
 	const contentList = document.getElementById(UNCOMPLETED_LIST_DATA_ID);
 
-	if (isCompleted == false) {
-		console.log("OK")
-	}
-
 	const textTitle = document.createElement("h3");
 	textTitle.innerText = title;
 	
-	const ilogo = document.createElement("i");
-	ilogo.classList.add("fas" ,"fa-trash-alt");
+	const ilogoTrash = document.createElement("i");
+	ilogoTrash.classList.add("fas" ,"fa-trash-alt");
+
+	const ilogoThumbtack = document.createElement("i");
+	ilogoThumbtack.classList.add("fas" ,"fa-thumbtack");
+
+	const logoBox = document.createElement("div");
+	logoBox.classList.add("aksi-box");
+	if (isCompleted == false) {
+		logoBox.append(ilogoThumbtack);
+	}
+	logoBox.append(ilogoTrash);
 
 	const textp = document.createElement("p");
 	textp.innerText = pengarang;
@@ -44,7 +50,7 @@ function buatData(title, pengarang, date, isCompleted) {
 
 	const textBox = document.createElement("div");
 	textBox.classList.add("box");
-	textBox.append(textTitle, textp, textDate, ilogo);
+	textBox.append(textTitle, textp, textDate, logoBox);
 
 	contentList.appendChild(textBox);
 }
@@ -74,7 +80,7 @@ function loadDataFromStorage() {
 
 	if (data !== null)
 		datas = data;
-		console.log("datas terisi");
+		console.log("loadDataFromStorage Success");
 
 	document.dispatchEvent(new Event("ondataloaded"));
 }
